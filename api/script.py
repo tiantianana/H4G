089d3676ded055ab9e6804a67a2dc1f67de2ad3b
tiantianana <tiantian.ana@gmail.com>
2022-10-22 09:51:56 +0200

    api

---
+++ api/script.py
@@ -0,0 +1,11 @@
+from pydub import AudioSegment
+import sys
+from scipy.io import wavfile
+import numpy as np
+
+scale = .8
+sound = AudioSegment.from_mp3("./resources/" + sys.argv[1])
+sound.export("temp.wav", format="wav")
+sample_rate, data = wavfile.read('temp.wav')
+wavfile.write("temp.wav", int(sample_rate*scale),data)
+AudioSegment.from_wav("temp.wav").export("./resources/" + '.'.join(sys.argv[1].split('.')[:-1]) + '_processed.mp3', format='mp3')
---
+++ api/src/controller/file.controller.js
@@ -0,0 +1,81 @@
+const uploadFile = require("../middleware/upload");
+var cmd = require('node-cmd');
+const fs = require('fs');
+
+const upload = async (req, res) => {
+
+    try {
+        await uploadFile(req, res);
+
+        if (req.file == undefined) {
+            return res.status(400).send({ message: "Please upload a file!" });
+        }
+
+        var pyProcess = cmd.run('python script.py ' + '"'+ req.file.originalname + '"' ,
+            function (data, err, stderr) {
+                if (!err) {
+                    console.log("data from python script " + data)
+                } else {
+                    console.log("python script cmd error: " + err)
+                }
+            }
+        );
+
+        res.status(200).send({
+            message: "Uploaded the file successfully: " + req.file.originalname,
+        });
+
+    } catch (err) {
+        if (err.code == "LIMIT_FILE_SIZE") {
+            return res.status(500).send({
+                message: "File size cannot be larger than 4MB!",
+            });
+        }
+
+        res.status(500).send({
+            message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+        });
+    }
+};
+
+const getListFiles = (req, res) => {
+    const directoryPath = __basedir + "/resources/";
+
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            res.status(500).send({
+                message: "Unable to scan files!",
+            });
+        }
+
+        let fileInfos = [];
+
+        files.forEach((file) => {
+            fileInfos.push({
+                name: file
+               // url: baseUrl + file,
+            });
+        });
+
+        res.status(200).send(fileInfos);
+    });
+};
+
+const download = (req, res) => {
+    const fileName = req.params.name;
+    const directoryPath = __basedir + "/resources/";
+
+    res.download(directoryPath + fileName, fileName, (err) => {
+        if (err) {
+            res.status(500).send({
+                message: "Could not download the file. " + err,
+            });
+        }
+    });
+};
+
+module.exports = {
+    upload,
+    getListFiles,
+    download,
+};
